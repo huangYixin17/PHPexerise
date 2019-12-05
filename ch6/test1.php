6fed1697d567bd44df803b0a011142e5c06bc7c8
xiz860817 <s0984008299@gmail.com>
2019-12-05 21:41:18 +0800

    更新

--- ch6/test1.php
+++ ch6/test1.php
@@ -19,7 +19,7 @@
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
+      <li><a href="#">123 1</a></li>
       <li><a href="#">Page 2</a></li>
       <li><a href="#">Page 3</a></li>
     </ul>
