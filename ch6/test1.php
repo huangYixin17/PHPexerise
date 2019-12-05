0f9ab38979bead3ed9fbcc97cf8dcf23a769b687
xiz860817 <s0984008299@gmail.com>
2019-12-05 21:36:53 +0800

    更新排版

--- ch6/test1.php
+++ ch6/test1.php
@@ -12,16 +12,27 @@
     <title>Hello, world!</title>
 </head>
 <body>
-<form class="navbar-form navbar-left" action="/action_page.php">
-  <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search">
-    <div class="input-group-btn">
-      <button class="btn btn-default" type="submit">
-        <i class="glyphicon glyphicon-search"></i>
-      </button>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
   </div>
-</form>
+</nav>
     <h1>Hello, world!</h1>
 
     <!-- Optional JavaScript -->
