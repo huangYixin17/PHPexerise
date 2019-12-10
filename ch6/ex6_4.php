876465279b6e6badf4a375d21b61a0405ce3e558
xiz860817 <s0984008299@gmail.com>
2019-12-10 19:47:37 +0800

    新增

--- ch6/ex6_4.php
+++ ch6/ex6_4.php
@@ -2,12 +2,12 @@
  $x = 1;
  function hello($x){
        $sum = $x * 2;
-       echo "函式執行結束 <br />";
+       echo "$sum<br/>函式執行結束 <br />";
  }
 ?>
 <!-----------------註解-------------->
 <?php
  hello($x);
  echo "x=$x<br />";
- echo "sum =$sum <br />";
+ echo "sum=$sum <br />";
 ?>
