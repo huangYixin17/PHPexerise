420f76361a27e83e7a53d25c1fcddbce5bab81c2
xiz860817 <s0984008299@gmail.com>
2019-12-10 19:41:50 +0800

    新增

--- ch6/ex6_4.php
+++ ch6/ex6_4.php
@@ -1,13 +1,13 @@
 <?php
-      $x = 1;
-      function hello($x){
-        $sum = $x * 2;
-        echo "函數執行結束 <br />";
-      }
+ $x = 1;
+ function hello($x){
+       $sum = $x * 2;
+       echo "函式執行結束 <br/>";
+ }
 ?>
-    <p> -------我是分隔線------</p>
+<!-----------------註解-------------->
 <?php
-      hello($x);
-      echo "x=$x<br />";
-      echo "sum=$sum<br />";
+ hello($x);
+ echo "x=$x<br/>";
+ echo "sum = $sum <br/>";
 ?>
