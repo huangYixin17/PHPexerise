7c0f3d61efa2fc938594b512c4229e25fe04c21f
xiz860817 <s0984008299@gmail.com>
2019-12-10 21:54:26 +0800

    新增

---
+++ ex6_10.php
@@ -0,0 +1,18 @@
+<?php
+ //一維陣列
+ $eggbox["土雞"] = 30;
+ $eggbox[1] = 100;
+ $eggboxp[] = 20;
+ $drinkbox=["啤酒"=>70,"cola"=>30];
+ $foodbox = ['飯糰'=>50];
+ echo $eggbox[1];
+ echo $drinkbox[1];
+ echo $foodbox;
+ //二維陣列
+ $lunch[0]["素食"]=100;
+ $lunch[0]["葷食"]=150;
+ $lunch['麵類']['拉麵']=200;
+ $lunch['飯類']['雞肉飯'] ="今天不賣";
+ $lunch[1]=array("廣東粥"=>80,"雞排"=>70);
+ echo $lunch;
+?>
