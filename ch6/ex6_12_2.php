9d96e2fe9c1b0e7300fa8f3fcab9db5b3f79d024
xiz860817 <s0984008299@gmail.com>
2019-12-12 19:59:46 +0800

    更新

---
+++ ch6/ex6_12_2.php
@@ -0,0 +1,7 @@
+<?php
+ $sum =0;
+ for($i=0;i<=50;i++){
+     $sum = $sum+1;
+ }
+ echo "sum = $sum";
+?>
---
+++ ch6/ex6_12_3.php
@@ -0,0 +1,17 @@
+<?php
+ $sum = 0;
+ $i = 0;
+ while($i<=50){
+     $sum = $sum+1;
+     $i++;
+ }
+ echo "sum= $sum";
+
+ $sum = 0;
+ $i = 0;
+ do{
+     $sum = $sum+1;
+     $i++;
+ }while($i<=50);
+ echo "sum= $sum";
+?>
