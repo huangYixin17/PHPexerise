c26c5cc4c524c41600e5d1902c4e46849a5ed3d8
xiz860817 <s0984008299@gmail.com>
2019-12-12 20:55:11 +0800

    function用法

---
+++ ch7/ex7_1.php
@@ -0,0 +1,30 @@
+<?php
+function name(){
+    echo "peter";
+}
+function score($i){
+    $j = "F";
+    $level = intval($i/10);
+    switch($level){
+        case 10:
+        case 9:
+            $j = "A";
+            break;
+        case 8:
+            $j = "B";
+            break;
+        case 7:
+            $j = "C";
+            break;
+        case 6:
+            $j = "D";
+            break;
+        default:
+            $j = "E";
+    }
+    return $j;
+}
+echo name();
+$backscore = score(85);
+echo " 成績等級: $backscore";
+?>
