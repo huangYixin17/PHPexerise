0305582f95f2218f7f2809839f6335064e9e7a34
xiz860817 <s0984008299@gmail.com>
2019-12-12 21:14:24 +0800

    foreach註解

--- ch7/ex7_2.php
+++ ch7/ex7_2.php
@@ -1,5 +1,10 @@
 <?php
-function sum(...$numbers){
+/*如果不知道被引入的參數有幾個
+可以用...來帶入
+foreach表示把所有numbers內的變數都執行完
+(變數1 as 變數2 ;將變數2 = 變數1)
+*/
+function sum(...$numbers){ 
     $total = 0;
     foreach($numbers as $i){
         $total = $total + $i;
