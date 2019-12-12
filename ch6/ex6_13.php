8acb6060b683bfe627fe03b096b3563c647bb438
xiz860817 <s0984008299@gmail.com>
2019-12-12 20:24:04 +0800

    新增

--- ch6/ex6_13.php
+++ ch6/ex6_13.php
@@ -1,8 +1,8 @@
 <?php
  for($i = 2;$i<10;$i++){
-     for($j = 1;$j<10;$j++)(
+     for($j = 1;$j<10;$j++){
          echo "%d*%d=%d \t",$i,$j,($i*$j);
-     )
-     echo "\n"
+    }
+     echo "\n";
  }
 ?>
