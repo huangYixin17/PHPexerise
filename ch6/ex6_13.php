9d88ba59171d17e3a4a28de6f012bb5ef5781f58
xiz860817 <s0984008299@gmail.com>
2019-12-12 20:31:29 +0800

    修改

--- ch6/ex6_13.php
+++ ch6/ex6_13.php
@@ -1,8 +1,8 @@
 <?php
 for($i = 2;$i<10;$i++){
     for($j = 1;$j<10;$j++){
-        printf ("%d*%d=%d \t",$i,$j,($i*$j));
+        printf("%d*%d=%d \t",$i,$j,($i*$j));
     }
-    printf();
+    print("\n");
 }
 ?>
