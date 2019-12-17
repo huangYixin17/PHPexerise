f6b6f9f35bce0a32b7b837f7de9f9577b1838199
xiz860817 <s0984008299@gmail.com>
2019-12-17 19:09:23 +0800

    新增

--- ch6/ex6_4.php
+++ ch6/ex6_4.php
@@ -1,13 +1,13 @@
 <?php
- $x = 1;
- function hello($x){
-       $sum = $x * 2;
-       echo "$sum 函式執行結束 <br />";
- }
+      $x = 1;
+      function hello($x){
+        $sum = $x * 2;
+        echo "函數執行結束 <br />";
+      }
 ?>
-<!-----------------註解-------------->
+    <p> -------我是分隔線------</p>
 <?php
- hello($x);
- echo "x=$x<br />";
- echo "sum=$sum <br />";
+      hello($x);
+      echo "x=$x<br />";
+      echo "sum=$sum<br />";
 ?>
--- ch6/ex6_5.php
+++ ch6/ex6_5.php
@@ -1,7 +1,4 @@
 <?php
-
- define('PI',3.1415926);
-
- $circle_area = 3 * 3 * PI;
- echo "圓面積: ".$circle_area;
+      define('PI', 3.1415926);
+      echo "圓周率：".PI;
 ?>
