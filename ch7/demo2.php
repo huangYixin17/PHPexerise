6d6654cd9751c37b1cb42ed01a6d927001d310d2
xiz860817 <s0984008299@gmail.com>
2019-12-17 21:11:25 +0800

    class複寫

---
+++ ch7/demo2.php
@@ -0,0 +1,6 @@
+<?php
+    include "override.php";
+
+    $mydog = new override("阿醜","綠色","貴賓狗","3吋");
+    echo $mydog->dogBark();
+?>
---
+++ ch7/override.php
@@ -0,0 +1,18 @@
+<?php
+    include "dog.php";
+
+    class override extends dog{
+        public $size;
+
+        public function __construct($name,$color,$style,$size)
+        {
+            parent::__construct($name,$color,$style);
+            $this->size = $size;
+        }
+        public function dogBark()
+        {
+            echo "狗叫...但小聲...";
+        }
+    }
+
+?>
