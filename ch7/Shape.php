6b85c9326a4d66bf83c2f0df2d87f63cdf03676d
xiz860817 <s0984008299@gmail.com>
2019-12-19 21:46:03 +0800

    方法的多載

---
+++ ch7/Shape.php
@@ -0,0 +1,24 @@
+<?php
+    class Shape{
+        const PI = 3.141592653;
+
+        function __call($name, $arg)
+        {
+            if($name == 'area')
+                switch(count($age)){
+                    case 0:return 0;
+                    case 1:return self::PI * $arg[0];
+                    case 2:return self::PI * $arg[1];
+                }
+        }
+        function __callStatic($name, $arg)
+        {
+            return array(3,5);
+        }
+    }
+    $circle = new Shape();
+    echo $circle->area(3);
+    $rent = new Shape();
+    echo $rent->area(8,6);
+    echo Shape::test()[0];
+?>
