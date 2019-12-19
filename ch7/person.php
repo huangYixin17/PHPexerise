090d52301f984da1c74231a50a35ae8e000809b5
xiz860817 <s0984008299@gmail.com>
2019-12-19 21:28:01 +0800

    test

--- ch7/person.php
+++ ch7/person.php
@@ -1,26 +1,29 @@
 <?php
-    class person{
-        private $sex;
-        /*
-        public function __set($name,$value){
+  class Person {
+      private $sex;
+      public function __set($name, $value){
         
-            if(isset($this->name)){
-                return $this->$name = $value;
-            }else{
-                return null;
-            }
+        //限制不可動態產生屬性
+        if (isset($this->$name)) {
+            return $this->$name = $value;
+        } else {
+            return null;
         }
-        */
-    }
-    public function __get($name){
+      }
+      // 取得屬性名稱的值
+      public function __get($name){
         return $name;
-    }
-    public function __isset($name){
+      }
+      // 判斷是否有該屬性參數   
+      public function __isset($name){
         return $name;
+      }
+      // 取消該屬性
+      public function __unset($name){
+        return $name;
+      }
     }
-    public function __unset($name){
-        return name;
-    }
+    
     $person = new Person();
     //Person 類別沒有 name 這個屬性名稱
     //PHP_EOL 空隔或是換行
@@ -30,5 +33,4 @@
     echo $person->sex.PHP_EOL;
     echo isset($person->address);
     unset($person->name);
-
 ?>
