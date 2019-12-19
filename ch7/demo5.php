90b8934d4a6f37911c680f1eb7e7e951fcbecfb3
xiz860817 <s0984008299@gmail.com>
2019-12-19 20:55:55 +0800

    test

--- ch7/demo5.php
+++ ch7/demo5.php
@@ -1,22 +1,22 @@
 <?php
-  abstract class ABC {
+  abstract class Aminal {
   public function sleep(){
       echo '睡';
   }
 }
-class Dog extends ABC {
+class Dog extends Aminal {
   public function move(){
       echo '跑';
   }
 }
-class Bird extends ABC{
+class Bird extends Aminal{
   public function move()
   {
       echo '飛';
   }
 }
 class Action{
-  public function actionMove(ABC $obj)
+  public function actionMove(Aminal $obj)
   {
       $obj->move();
   }
