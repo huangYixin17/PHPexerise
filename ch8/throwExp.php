68f9171d9f72e10442f9e8affcc4d0e67cf378e7
xiz860817 <s0984008299@gmail.com>
2020-01-07 21:06:26 +0800

    例外處理

---
+++ ch8/throwExp.php
@@ -0,0 +1,14 @@
+<?php
+    function check($num){
+        if ($num == 0){
+            throw new Exception("num can not be 0",100);
+        }
+        return $num;
+    }
+    try{
+        echo check(0);
+    }catch(Exception $e){
+        echo "Error Message:".$e->getMessage()."<br/>";
+        echo "Error Code: ".$e->getCode()."<br/>";
+    }
+?>
