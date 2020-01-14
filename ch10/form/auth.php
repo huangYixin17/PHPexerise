6183f6d4d8865c09bdc63c55e09ce86b4fa81680
xiz860817 <s0984008299@gmail.com>
2020-01-14 20:51:39 +0800

    test

--- ch10/form/auth.php
+++ ch10/form/auth.php
@@ -2,7 +2,7 @@
 $account = $_POST["account"];
 $password = $_POST["password"];
 if($account != 123 or $password!= 1234){
-    echo "<script>alert(‘提示內容’)</script>";
+    echo "<script>alert(‘提示內容’)</script>";}
 else{
     echo "登入成功!"
 }
