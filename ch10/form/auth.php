329e4147908b849bceaed3e5bf0c5d193f8625ef
xiz860817 <s0984008299@gmail.com>
2020-01-14 20:46:27 +0800

    test

--- ch10/form/auth.php
+++ ch10/form/auth.php
@@ -1,7 +1,12 @@
 <?php
 $account = $_POST["account"];
 $password = $_POST["password"];
+if($account != 123){
+    echo "帳號或是密碼輸入錯誤!!";
+}
+else{
+    echo "帳號 : ".$account;
+    echo "密碼 : ".$password;
+}
 
-echo "帳號 : ".$account;
-echo "密碼 : ".$password;
 ?>
